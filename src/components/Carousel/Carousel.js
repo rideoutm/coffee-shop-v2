53fb6782177a05781725b72c6a4d47e0d0a41920
Michael Rideout <rideout.m@hotmail.com>
2022-10-23 21:27:18 -0400

    fix carousel scroll & text animations w/ setTimeout

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -9,21 +9,24 @@ const slideData = [
   {
     id: 0,
     image: image1,
-    maintext: "ROASTED TO",
+    maintext1: "ROASTED TO",
+    maintext2: "PERFECTION",
     subtext:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Voluptates, facilis?",
   },
   {
     id: 1,
     image: image2,
-    maintext: "ONLY THE FINEST BEANS",
+    maintext1: "ONLY THE",
+    maintext2: "FINEST BEANS",
     subtext:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Voluptates, facilis?",
   },
   {
     id: 2,
     image: image3,
-    maintext: "THE BEST PLACE TO UNWIND",
+    maintext1: "COME TO",
+    maintext2: "SIT & UNWIND",
     subtext:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Voluptates, facilis?",
   },
@@ -31,26 +34,58 @@ const slideData = [
 
 export default function Carousel() {
   const [index, setIndex] = useState(0);
+  const [animation, setAnimation] = useState("carousel__main-text--anim");
+  const [subTextAnimation, setSubTextAnimation] = useState(
+    "carousel__sub-text--anim"
+  );
+  const [btnAnimation, setBtnAnimation] = useState("carousel__btn--anim");
 
   let carouselScroll = () => {
     console.log(index);
     if (index === slideData.length - 1) {
-      return setIndex(0);
+      return (
+        setIndex(0),
+        setTimeout(() => {
+          setAnimation("carousel__main-text--anim");
+          setSubTextAnimation("carousel__sub-text--anim");
+          setBtnAnimation("carousel__btn--anim");
+        }, 500),
+        setTimeout(() => {
+          setAnimation("carousel__main-text");
+          setSubTextAnimation("carousel__sub-text");
+          setBtnAnimation("carousel__btn");
+        }, 5000)
+      );
     }
 
-    return setIndex(index + 1);
+    return (
+      setIndex(index + 1),
+      setTimeout(() => {
+        setAnimation("carousel__main-text--anim");
+        setSubTextAnimation("carousel__sub-text--anim");
+        setBtnAnimation("carousel__btn--anim");
+      }, 500),
+      setTimeout(() => {
+        setAnimation("carousel__main-text");
+        setSubTextAnimation("carousel__sub-text");
+        setBtnAnimation("carousel__btn");
+      }, 6500)
+    );
   };
-  console.log(index);
 
   useEffect(() => {
     const interval = setInterval(() => {
       carouselScroll();
-    }, 10000);
+    }, 7000);
 
     return () => {
       clearInterval(interval);
     };
-  });
+  }, [index]);
+
+  // useEffect(() => {
+  //   setAnimation(animation);
+  // }, [index]);
 
   return (
     <div className="carousel">
@@ -62,16 +97,36 @@ export default function Carousel() {
               background: `url(${el.image}) center / cover no-repeat`,
               transform: `translate(-${index * 100}%)`,
             }}
-            key={i}
+            key={el.id}
           >
             <div className="carousel__text-container">
-              <div className={`carousel__main-text-${i}`}>{el.maintext}</div>
-              <div className="carousel__sub-text">{el.subtext}</div>
-              <button className="carousel__btn">READ MORE</button>
+              <div key={el.id} className={animation}>
+                {el.maintext1} <p>{el.maintext2}</p>
+              </div>
+
+              <div className={subTextAnimation}>{el.subtext}</div>
+              <button className={btnAnimation}>READ MORE</button>
             </div>
           </div>
         );
       })}
+      <div className="carousel__dot-container">
+        <div
+          onClick={() => setIndex(0)}
+          className={index === 0 ? "carousel__dot--active" : "carousel__dot"}
+        ></div>
+        <div
+          onClick={() => {
+            setIndex(1);
+            setAnimation(animation);
+          }}
+          className={index === 1 ? "carousel__dot--active" : "carousel__dot"}
+        ></div>
+        <div
+          onClick={() => setIndex(2)}
+          className={index === 2 ? "carousel__dot--active" : "carousel__dot"}
+        ></div>
+      </div>
     </div>
 
     // <div className="carousel">
