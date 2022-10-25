8b55f9bee7da727971e8798247237f042f436c96
Michael Rideout <rideout.m@hotmail.com>
2022-10-24 21:58:06 -0400

    add header scroll transparency anim

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Modal from "./components/Modal/Modal";
 import { useState } from "react";
-import Hero from "./components/Hero/Hero";
 import Carousel from "./components/Carousel/Carousel";
 
 function App() {
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,29 @@
 import "./Header.scss";
+import coffeeshoplogo from "../../Data/imgs/COFFEESHOPLOGO2.png";
+
+import { useState, useEffect } from "react";
 
 export default function Header({ handleModal }) {
+  const [headerState, setHeaderState] = useState(false);
+
+  const changeBackground = () => {
+    if (window.scrollY > 200) {
+      setHeaderState(true);
+    } else {
+      setHeaderState(false);
+    }
+  };
+
+  useEffect(() => {
+    changeBackground();
+    window.addEventListener("scroll", changeBackground);
+  });
+
   return (
-    <div className="header">
-      <div className="header__logo">COFFEE SHOP</div>
+    <div className={!headerState ? "header" : "header--scroll"}>
+      <div className="header__logo">
+        <img src={coffeeshoplogo} alt="logo"></img>
+      </div>
       <div onClick={() => handleModal()} className="header__ham-menu">
         <div className="header__top-line"></div>
         <div className="header__mid-line"></div>
