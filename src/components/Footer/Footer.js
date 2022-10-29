d3af4928ee85aec20ad613d2de82d4090c40948f
Michael Rideout <rideout.m@hotmail.com>
2022-10-29 19:07:04 -0400

    begin tablet layout

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,65 @@
 import "./Footer.scss";
 
 export default function Footer() {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <div className="footer__store-hours">
+        <h2 className="footer__header">STORE HOURS</h2>
+        <div className="footer__store-list">
+          <div className="footer__store-list-item">
+            <span className="footer__store-hours-day">MONDAY</span>6AM - 11PM
+          </div>
+          <div className="footer__store-list-item">
+            <span className="footer__store-hours-day">TUESDAY</span>6AM - 11PM
+          </div>
+          <div className="footer__store-list-item">
+            <span className="footer__store-hours-day">WEDNESDAY</span>6AM - 11PM
+          </div>
+          <div className="footer__store-list-item">
+            <span className="footer__store-hours-day">THURSDAY</span>6AM - 11PM
+          </div>
+          <div className="footer__store-list-item">
+            <span className="footer__store-hours-day">FRIDAY</span>6AM - 10PM
+          </div>
+          <div className="footer__store-list-item">
+            <span className="footer__store-hours-day">SATURDAY</span>6AM - 7PM
+          </div>
+          <div className="footer__store-list-item">
+            <span className="footer__store-hours-day">SUNDAY</span>{" "}
+            <span className="footer__store-hours-closed">CLOSED</span>
+          </div>
+          <div></div>
+        </div>
+      </div>
+      <div className="footer__posts">
+        <h2 className="footer__posts-header">BLOG POSTS</h2>
+        <ul className="footer__posts-list">
+          <li className="footer__posts-item">
+            SEIZE THE DAY, BUT FIRST COFFEE <br />{" "}
+            <span className="footer__posts-item-date">01/05/2022</span>
+          </li>
+          <li className="footer__posts-item">
+            WITH ALL GOOD THINGS, COFFEE <br />{" "}
+            <span className="footer__posts-item-date">01/05/2022</span>
+          </li>
+          <li className="footer__posts-item">
+            HOW TO MAKE THE PERFECT CUP <br />{" "}
+            <span className="footer__posts-item-date">01/05/2022</span>
+          </li>
+        </ul>
+      </div>
+      <div className="footer__contact">
+        <h2 className="footer__contact-header">CONTACT US</h2>
+        <ul className="footer__list">
+          <li className="footer__list-item">
+            <a className="footer__list-item--link" href="mailto: #">
+              coffee@coffeeshop.com
+            </a>
+          </li>
+          <li className="footer__list-item">+1-(123)-4567</li>
+          <li className="footer__list-item">123 st. Toronto, ON</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
--- src/components/HpBlogComp/HpBlogComp.js
+++ src/components/HpBlogComp/HpBlogComp.js
@@ -1,6 +1,7 @@
 import "./HpBlogComp.scss";
 import blog1 from "../../Data/imgs/pexels-anna-urlapova-2956954.jpg";
 import blog2 from "../../Data/imgs/nathan-dumlao-KixfBEdyp64-unsplash.jpg";
+import blog3 from "../../Data/imgs/image3.jpg";
 
 export default function HpBlogComp() {
   return (
@@ -33,7 +34,24 @@ export default function HpBlogComp() {
         </div>
 
         <div className="homepage-blog__item-desc">
-          <h3 className="homepage-blog__item-title">THE BEST ARABICA</h3>
+          <h3 className="homepage-blog__item-title">ARABICA</h3>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat
+          enim cupiditate quia molestiae dolor laborum animi sed dolorum porro
+          numquam!
+        </div>
+        <button className="homepage-blog__btn">READ MORE</button>
+      </div>
+      <div className="homepage-blog__item">
+        <div className="homepage-blog__item-img">
+          <img
+            className="homepage-blog__item-img-pic"
+            src={blog3}
+            alt=" blog post one"
+          />
+        </div>
+
+        <div className="homepage-blog__item-desc">
+          <h3 className="homepage-blog__item-title">OUR GRIND</h3>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat
           enim cupiditate quia molestiae dolor laborum animi sed dolorum porro
           numquam!
