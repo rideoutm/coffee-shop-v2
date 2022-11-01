b40543dbb2cacebad454e64c3b91d7b6f3356dd9
Michael Rideout <rideout.m@hotmail.com>
2022-11-01 16:24:43 -0400

    add partner logos

--- src/components/Partners/Partners.js
+++ src/components/Partners/Partners.js
@@ -1,4 +1,37 @@
 import "./Partners.scss";
+import awakeCoffee from "../../Data/imgs/AwakeCoffee.svg";
+import barcelle from "../../Data/imgs/barcelle.svg";
+import coffeeBean from "../../Data/imgs/coffeeBean.svg";
+import expressCoffee from "../../Data/imgs/expresscoffee.svg";
+import freshRoasted from "../../Data/imgs/freshRoasted.svg";
+import coffeeshop2012 from "../../Data/imgs/coffeeShop2012.svg";
+
+const imageData = [
+  {
+    image: awakeCoffee,
+    alt: "partner logo one",
+  },
+  {
+    image: barcelle,
+    alt: "partner logo two",
+  },
+  {
+    image: coffeeBean,
+    alt: "partner logo three",
+  },
+  {
+    image: expressCoffee,
+    alt: "partner logo four",
+  },
+  {
+    image: freshRoasted,
+    alt: "partner logo five",
+  },
+  {
+    image: awakeCoffee,
+    alt: "partner logo six",
+  },
+];
 
 export default function Partners() {
   return (
@@ -12,6 +45,13 @@ export default function Partners() {
           earum.
         </div>
       </div>
+      {imageData.map((el) => {
+        return (
+          <div>
+            <img className="partners__logo" src={el.image} alt={el.alt} />
+          </div>
+        );
+      })}
       <div className="partners__right"></div>
     </div>
   );
