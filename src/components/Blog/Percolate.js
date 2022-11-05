b456ac25bf951ebb7c78dd14207b5f9737683609
Michael Rideout <rideout.m@hotmail.com>
2022-11-05 17:18:23 -0400

    build tablet & desktop blog layout blog pg

--- src/components/Blog/Percolate.js
+++ src/components/Blog/Percolate.js
@@ -32,105 +32,109 @@ export default function Blog() {
 
   return (
     <div className="blog">
-      <img className="blog__title-img" src={titleImg} alt="title image" />
+      <div className="blog__left">
+        <img className="blog__title-img" src={titleImg} alt="title image" />
 
-      <div className="blog__info">
-        by <span className="blog__info-author">Meghan Smith</span> |{" "}
-        <span className="blog__info-date"> 10/27/2022</span>
-      </div>
-      <div className="blog__article">
-        <h2 className="blog__article-title">PERCOLATE</h2>
-        <div className="blog__article-content">
-          <p className="blog__article-para">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis
-            quis at quisquam! Obcaecati animi nisi, reiciendis cumque maiores ea
-            error provident suscipit quam quae non quia iusto vero quas quod sed
-            eaque neque architecto reprehenderit deserunt a laudantium
-            distinctio.
-          </p>
-          <p className="blog__article-para">
-            Doloremque consequuntur atque quas maiores odit ipsam perferendis.
-            Fugit, minima voluptatum quasi consequuntur, maiores vitae assumenda
-            quae, esse sed ex dolor rerum impedit placeat. Fugit fuga a expedita
-            doloremque sed, laboriosam at vero voluptate quod, ad ab
-            exercitationem facere qui, delectus non enim placeat odit. Beatae
-            ullam expedita vel, perspiciatis quam nobis eius corrupti. Aut quia
-            nam dignissimos voluptatibus nemo quaerat earum, magnam voluptatum
-            consectetur pariatur et numquam, ullam odio.{" "}
-          </p>{" "}
-          <p className="blog__article-para">
-            {" "}
-            Vitae hic quibusdam illum, fuga totam necessitatibus modi, voluptate
-            aperiam veritatis minima ratione labore doloremque quod iusto
-            debitis, velit perferendis praesentium! Dolores reiciendis ut
-            similique doloribus aut numquam eligendi ad ipsam repudiandae et
-            fugiat enim ea, fugit dignissimos dolore perspiciatis accusantium.
-            Dignissimos minus, repudiandae modi quia quae hic enim debitis
-            accusamus delectus facilis doloremque sed distinctio architecto quod
-            facere aspernatur eius, iusto maxime sunt.
-          </p>
-          <p className="blog__article-para">
-            Eveniet fugiat repellat tempora necessitatibus beatae nulla ullam,
-            sapiente voluptates labore illum deserunt, deleniti aliquid cum
-            animi doloribus tempore officiis, aut culpa hic ut consequatur
-            voluptatum iste!
-          </p>
+        <div className="blog__info">
+          by <span className="blog__info-author">Meghan Smith</span> |{" "}
+          <span className="blog__info-date"> 10/27/2022</span>
         </div>
-        <hr className="blog__end-line" />
-        <div className="blog__author-about">
-          <img
-            className="blog__author-img"
-            src={authorImg}
-            alt="image of author"
-          />
-          <h3 className="blog__author-name">Meghan Smith</h3>
-          <div className="blog__author-desc">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti,
-            alias quae. Commodi perspiciatis nisi quis deserunt, laudantium ipsa
-            modi debitis?
-            <hr className="blog__author-btm-hr" />
+        <div className="blog__article">
+          <h2 className="blog__article-title">PERCOLATE</h2>
+          <div className="blog__article-content">
+            <p className="blog__article-para">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Blanditiis quis at quisquam! Obcaecati animi nisi, reiciendis
+              cumque maiores ea error provident suscipit quam quae non quia
+              iusto vero quas quod sed eaque neque architecto reprehenderit
+              deserunt a laudantium distinctio.
+            </p>
+            <p className="blog__article-para">
+              Doloremque consequuntur atque quas maiores odit ipsam perferendis.
+              Fugit, minima voluptatum quasi consequuntur, maiores vitae
+              assumenda quae, esse sed ex dolor rerum impedit placeat. Fugit
+              fuga a expedita doloremque sed, laboriosam at vero voluptate quod,
+              ad ab exercitationem facere qui, delectus non enim placeat odit.
+              Beatae ullam expedita vel, perspiciatis quam nobis eius corrupti.
+              Aut quia nam dignissimos voluptatibus nemo quaerat earum, magnam
+              voluptatum consectetur pariatur et numquam, ullam odio.{" "}
+            </p>{" "}
+            <p className="blog__article-para">
+              {" "}
+              Vitae hic quibusdam illum, fuga totam necessitatibus modi,
+              voluptate aperiam veritatis minima ratione labore doloremque quod
+              iusto debitis, velit perferendis praesentium! Dolores reiciendis
+              ut similique doloribus aut numquam eligendi ad ipsam repudiandae
+              et fugiat enim ea, fugit dignissimos dolore perspiciatis
+              accusantium. Dignissimos minus, repudiandae modi quia quae hic
+              enim debitis accusamus delectus facilis doloremque sed distinctio
+              architecto quod facere aspernatur eius, iusto maxime sunt.
+            </p>
+            <p className="blog__article-para">
+              Eveniet fugiat repellat tempora necessitatibus beatae nulla ullam,
+              sapiente voluptates labore illum deserunt, deleniti aliquid cum
+              animi doloribus tempore officiis, aut culpa hic ut consequatur
+              voluptatum iste!
+            </p>
+          </div>
+          <hr className="blog__end-line" />
+          <div className="blog__author-about">
+            <img
+              className="blog__author-img"
+              src={authorImg}
+              alt="image of author"
+            />
+            <h3 className="blog__author-name">Meghan Smith</h3>
+            <div className="blog__author-desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti,
+              alias quae. Commodi perspiciatis nisi quis deserunt, laudantium
+              ipsa modi debitis?
+              <hr className="blog__author-btm-hr" />
+            </div>
+          </div>
+          <Comment blogData={blogData} />
+          <div>
+            <h2 className="blog__form-header">POST A COMMENT</h2>
+            <form action="">
+              <label htmlFor="name"></label>
+              <input
+                className={
+                  nameState < 2 ? "blog__form-name--invalid" : "blog__form-name"
+                }
+                id="name"
+                type="text"
+                placeholder="Name"
+                // value={nameState}
+                onChange={() => setNameState(nameState + 1)}
+              />
+              <label htmlFor="email"></label>
+              <input
+                className="blog__form-email"
+                id="email"
+                type="text"
+                placeholder="Email"
+                onChange={() => setEmailState(emailState + 1)}
+              />
+              <label htmlFor="comment"></label>
+              <textarea
+                className="blog__form-comment"
+                name=""
+                id="comment"
+                cols="30"
+                rows="10"
+                placeholder="Comment"
+                onChange={() => setCommentState(commentState + 1)}
+              ></textarea>
+              <button className="blog__form-btn" type="submit">
+                SUBMIT
+              </button>
+            </form>
           </div>
         </div>
-        <Comment blogData={blogData} />
-        <div>
-          <h2 className="blog__form-header">POST A COMMENT</h2>
-          <form action="">
-            <label htmlFor="name"></label>
-            <input
-              className={
-                nameState < 2 ? "blog__form-name--invalid" : "blog__form-name"
-              }
-              id="name"
-              type="text"
-              placeholder="Name"
-              // value={nameState}
-              onChange={() => setNameState(nameState + 1)}
-            />
-            <label htmlFor="email"></label>
-            <input
-              className="blog__form-email"
-              id="email"
-              type="text"
-              placeholder="Email"
-              onChange={() => setEmailState(emailState + 1)}
-            />
-            <label htmlFor="comment"></label>
-            <textarea
-              className="blog__form-comment"
-              name=""
-              id="comment"
-              cols="30"
-              rows="10"
-              placeholder="Comment"
-              onChange={() => setCommentState(commentState + 1)}
-            ></textarea>
-            <button className="blog__form-btn" type="submit">
-              SUBMIT
-            </button>
-          </form>
-        </div>
       </div>
-      <Related />
+      <div className="blog__right">
+        <Related />
+      </div>
     </div>
   );
 }
--- src/components/Blog/Related/Related.js
+++ src/components/Blog/Related/Related.js
@@ -11,13 +11,55 @@ export default function Related() {
         </div>
         <div className="related__info">
           <div className="related__info-title">
-            <h4>TITLE NAME</h4>
+            <h4 className="related__info-title-header">TITLE NAME</h4>
           </div>
           <div className="related__info-desc">
             {" "}
             Lorem ipsum dolor sit amet consectetur, adipisicing elit.
           </div>
         </div>
+      </div>
+      <div className="related__item">
+        <div className="related__img-cont">
+          <img className="related__item-img" src={related} alt="" />
+        </div>
+        <div className="related__info">
+          <div className="related__info-title">
+            <h4 className="related__info-title-header">TITLE NAME</h4>
+          </div>
+          <div className="related__info-desc">
+            {" "}
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          </div>
+        </div>
+      </div>
+      <div className="related__item">
+        <div className="related__img-cont">
+          <img className="related__item-img" src={related} alt="" />
+        </div>
+        <div className="related__info">
+          <div className="related__info-title">
+            <h4 className="related__info-title-header">TITLE NAME</h4>
+          </div>
+          <div className="related__info-desc">
+            {" "}
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          </div>
+        </div>
+      </div>
+      <div className="related__categories">
+        <h2 className="related__categories-header">CATEGORIES</h2>
+        <ul className="related__list">
+          <li className="related__list-item">Preparation</li>
+          <li className="related__list-item">Farming</li>
+          <li className="related__list-item">Bean types</li>
+          <li className="related__list-item">Coffee courses</li>
+          <li className="related__list-item">Coffee Recipes</li>
+        </ul>
+      </div>
+      <div className="related__tags">
+        <h2 className="related__tags-header">TAGS</h2>
+        Latte, Espresso, Cappucino, Iced Coffee, Drip, Caramel Macchiato
       </div>
     </div>
   );
